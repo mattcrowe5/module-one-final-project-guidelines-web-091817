c539493c60a1625a005153752d43bef49c376b72
mattcrowe5 <crowe.matthew5@gmail.com>
2017-10-03 15:45:55 -0400

    made user/beer relations

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,6 @@
 class Beer < ActiveRecord::Base
   has_many :beer_ingredients
   has_many :ingredients, through: :beer_ingredients
+  has_many :userbeers
+  has_many :users, through: :userbeers
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
-  
+  has_many :userbeers
+  has_many :beers, through: :userbeers
 end
