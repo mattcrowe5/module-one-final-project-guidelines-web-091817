64fa430ee763305190ddbb8b3db8bdc3bbdfd1ba
mattcrowe5 <crowe.matthew5@gmail.com>
2017-10-04 10:41:54 -0400

    CLI edits for start, main menu, created functions to support main menu options

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,3 +3,6 @@ class Ingredient < ActiveRecord::Base
   has_many :beers, through: :beer_ingredients
 
 end
+
+
+ #sqlite3 development.db 
--- lib/cli.rb
+++ lib/cli.rb
@@ -3,6 +3,13 @@ class CLI
    def start_program
      puts "Welcome to Kegatron 5000!"
      puts "========================="
+     puts "Please enter your name!"
+     name = gets.chomp
+     @user = User.find_or_create_by(name: name)
+     main_menu
+   end
+
+   def main_menu
      puts "Main Menu"
      puts "========================="
      puts "1. View Beer Selection"
@@ -15,14 +22,18 @@ class CLI
 
      input = gets.chomp
      case input
-     when 1
-       #Call method to provide list of beers
-     when 2
-       #call method to view list of favorite beers
-     when 3
-       #Call method to add a beer
-     when 4
-       #Call method to end program
+     when '1'
+       list_beers
+       user_beer_choice
+       beer_details
+     when '2'
+       @user.list_favs
+     when '3'
+       add_new_beer
+     when '4'
+       exit
+     else
+       select_valid_option
      end
    end
 
@@ -34,6 +45,60 @@ class CLI
        puts "#{counter}. #{beer.name}"
      end
    end
+
+   def user_beer_choice
+     beer_choice = gets.chomp
+     Beer.all.each do |beer|
+       if beer_choice == beer.id
+         @user_choice = beer
+       end
+     end
+   end
+
+   def beer_details
+     puts "#{@user_choice.name}"
+     puts "===================="
+     puts "1. Recipe"
+     puts "2. Description"
+     puts "3. Add to Favorites"
+     puts "4. Main Menu"
+
+     beer_details_choice = gets.chomp
+
+     case beer
+     when 1
+       puts "#{@user_choice.ingredients}"
+     when 2
+       puts "#{@user_choice.description}"
+     when 3
+       puts "#{@user.add_beer_to_favorites(@user_choice)}"
+     when 4
+       main_menu
+     else
+       select_valid_option
+     end
+
+   end
+
+
+   def add_new_beer
+     puts "Whats the name of your new beer?"
+     beer_name = gets.chomp
+     if Beer.list_beer_names.include?(beer_name)
+       puts "That beer already exists!"
+     else
+       new_beer = Beer.create(name: beer_name)
+     end
+     puts "Please add description of new beer"
+     response = gets.chomp
+     new_beer.description = response
+     main_menu
+   end
+
+   def select_valid_option
+     puts "Please select a valid option"
+   end
+
 
 
 
